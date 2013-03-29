15e69a945ff3bc8e0516913eb45328b1c5aa0d25
seba-1511 <seba-1511@hotmail.com>
2013-03-29 09:57:03 +0100

    Added config class file.

---
+++ includes/visualscience.config.class.php
@@ -0,0 +1,16 @@
+<?php 
+class Config {
+
+	private function getUserFields () {
+
+	}
+
+	private function getCreatedFields () {
+		
+	}
+
+	public function getHtmlConfgiPage () {
+		$userFields = this->getUserFields();
+		$otherFields = this->getCreatedFields();
+	}
+}
