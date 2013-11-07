ca402b66a130dbc2d41eb664b06105021ed88ccd
seba-1511 <seba-1511@hotmail.com>
2013-11-07 13:43:28 +0100

    Fixed some errors from code review.

--- includes/stringToCSV.php
+++ includes/stringToCSV.php
@@ -1,11 +1,14 @@
 <?php
+/**
+ * @file
+ * Receives a string through GET request and outputs a CSV file out if it.
+ */
 header('Content-type: text/csv');
-header('Content-Disposition: attachement;filename=livingscience_'.time().'.csv');
-$newLineCharacter = urldecode($_GET['char']);
+header('Content-Disposition: attachement;filename=livingscience_' . time() . '.csv');
+$new_line_character = urldecode($_GET['char']);
 // Fix for crappy IE bug in download.
 header('Pragma: ');
 header('Cache-Control: ');
 $text = urldecode($_GET['text']);
-$text = str_replace($newLineCharacter, "\r\n", $text);
+$text = str_replace($new_line_character, "\r\n", $text);
 echo htmlspecialchars($text);
-?>
